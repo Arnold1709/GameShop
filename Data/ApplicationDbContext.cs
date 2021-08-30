1995b5ec92433c50f9b7c2a259750d75882c1dee
Arnold1709 <arnold_guerrero1@usmp.pe>
2021-08-29 19:53:59 -0500

    Db Comtext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+namespace GameShop.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+                public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
